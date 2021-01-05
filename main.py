249f667bb8c4cf8ad6f8d680e1baaaa18ce321a8
Song Chen <43616275+songchen1999@users.noreply.github.com>
2021-01-05 17:14:16 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pickle
+from flask import Flask, request, jsonify
+from model_files.ml_model import predict_mpg
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def predict():
+    vehicle_config = request.get_json()
+    
+    with open('./model_files/model.bin','rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    
+    predictions = predict_mpg(vehicle_config, model)
+
+    response = {
+        'mpg_prediction': list(predictions)
+    }
+
+    return jsonify(response)
